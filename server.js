f5424765a39965cbe2498cb81b2f3a191d7f8adb
Dillon Kwak <dillon.esk@gmail.com>
2025-07-23 20:50:22 -0400

    first commit

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 /********************************************************************************
-*  WEB322 – Assignment 03
+*  WEB322 – Assignment 04
 * 
 *  I declare that this assignment is my own work in accordance with Seneca's
 *  Academic Integrity Policy:
 * 
 *  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Dillon Kwak Student ID: 135389203 Date: June 20th 2025
+*  Name: Dillon Kwak 
+*  Student ID: 135389203 
+*  Date: July 12th 2025
 *
 ********************************************************************************/
 
@@ -16,69 +18,58 @@ const app = express();
 const projectData = require("./modules/projects");
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs'); //.ejs will use EJS engine (templates)
 app.use(express.static('public')); //- reminder do not forget to mark the "public" folder as "static
 app.use(express.static(path.join(__dirname, "public")));
 projectData.initialize();
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.render("home", { page: "/" });
+
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/about.html"));
+    res.render("about", {page: "/about"});
 });
 
 app.get('/solutions/projects', (req, res) => {
     const sector = req.query.sector;
-    projectData.getAllProjects()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            res.send(err);
-        });
-    if(sector){
-        projectData.getProjectsBySector(sector)
-        .then((result) => {res.send(result);})
-        .catch((err) => {
-            res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
 
+    if (sector) {
+        projectData.getProjectsBySector(sector)
+        .then((projects) => {res.render("projects", {
+                projects: projects,
+                sector: sector,
+                page: "/solutions/projects"
+            });
+        }).catch((err) => {
+            res.status(404).render("404", {message: "Page not found", page: ""});
         });
     } else {
-        projectData.getAllProjects()
-        .then((result) => {res.send(result);})
-        .catch((err) => {
-            res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+        projectData.getAllProjects().then((projects) => {
+            res.render("projects", {
+                projects: projects,
+                page: "/solutions/projects"
+            });
+        }).catch((err) => {
+            res.status(404).render("404", {message: "Page not found", page: ""});
         });
     }
 });
 
 app.get('/solutions/projects/:id', (req, res) => {
     projectData.getProjectById(req.params.id)
-        .then((result) => {
-            res.send(result);
+        .then((project) => {
+            res.render("project-details", {project: project});
         })
         .catch((err) => {
-            res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+            res.status(404).render("404", { message: "No projects found for a specific id" });
             console.log("responded");
-
         });
 });
 
 app.set((req,res) => {
-    res.status(404).sendFile(path.join(__dirname, "/views/404.html"));
+    res.status(404).render("404", { message: "Page not found" });
 })
 
 app.listen(HTTP_PORT, () => console.log("Server responded"));
-
-
-//---------------Assignment 3 --------------//
-// app.get('/solutions/projects/sector-demo', (req, res) => {
-//     projectData.getProjectsBySector("agriculture")
-//         .then((result) => {
-//             res.send(result);
-//         })
-//         .catch((err) => {
-//             res.send(err);
-//         });
-// });
